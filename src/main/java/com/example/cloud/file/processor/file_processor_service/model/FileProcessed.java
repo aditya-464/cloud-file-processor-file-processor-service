96bd921e87e9ae891ab6a3701c751f1f4337f97a
Aditya Giri <adityagiri1911@gmail.com>
2025-12-05 06:47:48 +0530

    project initiated

---
+++ src/main/java/com/example/cloud/file/processor/file_processor_service/model/FileProcessed.java
@@ -0,0 +1,15 @@
+package com.example.cloud.file.processor.file_processor_service.model;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FileProcessed {
+}
